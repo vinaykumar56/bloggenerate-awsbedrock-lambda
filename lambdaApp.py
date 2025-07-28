9d7470f4b8510ce46ef37c422883e5ccccac238a
vinaykumar56 <innovate.code.vinay@gmail.com>
2025-07-28 16:38:19 +0530

    final changes

--- lambdaApp.py
+++ lambdaApp.py
@@ -3,6 +3,11 @@ import botocore.config
 import json
 import datetime
 
+#bedrockClient
+bedrockClient = boto3.client("bedrock-runtime", region_name="us-east-1", 
+config=botocore.config.Config(read_timeout=300,retries={'max_attempts':3}))
+
+s3Client=boto3.client('s3')
 
 def generate_blog_bedrock(topic:str)->str:
     #prompt
@@ -18,14 +23,13 @@ def generate_blog_bedrock(topic:str)->str:
         "top_p":0.9
     }
     try:
-        #bedrockClient
-        bedrockClient = boto3.client("bedrock-runtime", region_name="us-east-1", 
-                                     config=botocore.config.Config(read_timeout=300,retries={'max_attempt':3}))
         #model ID
         model_id = "meta.llama3-8b-instruct-v1:0" 
-        response = bedrockClient.invoke_model(body=json.dumps(body),model_id=model_id)
+        response = bedrockClient.invoke_model(body=json.dumps(body),modelId=model_id)
         response_content = json.loads(response.get('body').read())
         blog = response_content['generation']
+        print(f"blog generated for topic {topic}")
+        print(blog)
         return blog
     except Exception as e:
         print(f"Error in generating blog:{e}")
@@ -33,34 +37,34 @@ def generate_blog_bedrock(topic:str)->str:
 
 
 def save_blog_details_s3(bucket,key,blog):
-    s3Client=boto3.client('s3')
+    
     try:
-        s3Client.put_object(Buket = bucket, Key=key, Body=blog)
+        s3Client.put_object(Bucket = bucket, Key=key, Body=blog)
         print("blog text saved to s3")
     except Exception as e:
-        print("error saving blog file to s3 : {e}")    
+        print("error saving blog file to s3 :",e)    
 
 
 
 def lambda_handler(event, context):
+    print(event)
     event = json.loads(event['body'])
+    print(event)
     topic = event['blogTopic']
+    print(topic)
     blog = generate_blog_bedrock(topic=topic)
     if blog:
-        currentTime = datetime.now().strftime('%H%M%S')
+        currentTime = datetime.datetime.now().strftime('%H%M%S')
         s3Key = f"blogOutput/{topic}_{currentTime}.txt"
-        s3Bucket = 'aws_bedrock_test'
+        s3Bucket = 'vinay-balyan'
         save_blog_details_s3(s3Bucket,s3Key,blog)
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Blog generated and saved to S3')
+        }
     else:
         print("No blog was generated")
-
-
-
-
-
-
-
-
-
-
-
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Error generating blog')
+        }
